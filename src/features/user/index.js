b302129dbf487f98460916c2c413aef1c7314d4e
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-05 18:52:39 -0300

    :recycle: refactor: Change @/features/user exports

--- src/features/user/index.js
+++ src/features/user/index.js
@@ -1,6 +1,5 @@
+export { getUsers, getUserProfile } from './user-requests.js'
+export { useGetUsersQuery, useGetUserProfileQuery } from './user-api.js'
+
 export { default as UserList } from './UserList.jsx'
 export { default as User } from './User.jsx'
-
-export { useGetUsersQuery, useGetUserProfileQuery } from './user-service.js'
-
-export { getUsers, getUserProfile } from './user-requests.js'
